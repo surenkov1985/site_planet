40bed1eb2cbd3ba4bc3b24e49203dfc8bf61289c
Mikhail Surenkov <surenkov1985@gmail.com>
2023-10-11 17:51:44 +0300

    начало верстки главной страницы

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
     mainSlider();
+    consultantSlider();
+    partnersSlider();
 });
--- js/swipers.js
+++ js/swipers.js
@@ -11,7 +11,46 @@ function mainSlider () {
         mousewheel:false,
         // If we need pagination
         pagination: {
-            el: '.swiper-pagination',
+            el: '.fs__slider .swiper-pagination',
+            clickable:true
+        },
+    });
+}
+
+function consultantSlider () {
+    const mainSlider = document.querySelector('.consultants__slider');
+    if (!mainSlider) return;
+
+    const slider = new Swiper(mainSlider, {
+        loop: false,
+        rewind: true,
+        speed: 700,
+        slidesPerView: 2.5,
+        // allowTouchMove: false,
+        mousewheel:false,
+        // If we need pagination
+        pagination: {
+            el: '.consultants__slider_pagination',
+            clickable:true
+        },
+    });
+}
+
+function partnersSlider () {
+    const mainSlider = document.querySelector('.partners__slider');
+    if (!mainSlider) return;
+
+    const slider = new Swiper(mainSlider, {
+        loop: false,
+        rewind: true,
+        speed: 700,
+        slidesPerView: 4.2,
+        spaceBetween: 50,
+        // allowTouchMove: false,
+        mousewheel:false,
+        // If we need pagination
+        pagination: {
+            el: '.partners__slider_pagination',
             clickable:true
         },
     });
