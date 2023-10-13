5af474015912b5426c54237b24e27fab5ac64833
Mikhail Surenkov <surenkov1985@gmail.com>
2023-10-13 15:14:03 +0300

    верстка мобильного меню

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,50 @@ document.addEventListener("DOMContentLoaded", function(){
     consultantSlider();
     partnersSlider();
     newsSlider();
+
+    const scrollOptions = {
+		rootMargin: "100px",
+		threshold: 0.9,
+	};
+
+    const scrollCallback = (entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.boundingClientRect.y < 0) {
+				if (document.querySelector(".head-fixed")) {
+					// document.querySelector(".head").classList.add("fixed");
+                    document.querySelector(".head-fixed").classList.add("show");
+				}
+			} else {
+				if (document.querySelector(".head-fixed")) {
+					document.querySelector(".head-fixed").classList.remove("show");
+                    // document.querySelector(".head").classList.remove("fixed");
+				}
+			}
+		});
+	};
+    const observerTarget = document.querySelectorAll(".main section");
+    console.log(observerTarget);
+	const scrollObserver = new IntersectionObserver(scrollCallback, scrollOptions);
+	scrollObserver.observe(observerTarget[1]);
+	let points;
+    
+
+    $(document).on("click", ".head__nav_burger-btn", function () {
+        $(".mobile").addClass("show");
+        $("body").addClass("no-scroll");
+    
+    });
+
+    $(document).on("click", ".mobile__close_btn", function () {
+        $(".mobile").removeClass("show");
+        $("body").removeClass("no-scroll");
+    });
+    
+    $(document).on("click", ".mobile__nav_center button", function () {
+        $(".mobile__submenu").addClass("show");
+    });
+
+    $(document).on("click", ".submenu__close_btn", function () {
+        $(".mobile__submenu").removeClass("show");
+    });
 });
